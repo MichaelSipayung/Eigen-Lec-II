6ee4d8d95989e0860fc250799ba137f65509dec8
michael sipayung <michaelsipayung123@gmail.com>
2021-05-28 16:34:10 +0700

    Basic  Arithmetic Reduction Operations

--- Eigen Lec II/Eigen Lec II.cpp
+++ Eigen Lec II/Eigen Lec II.cpp
@@ -46,7 +46,63 @@ int main()
 	std::cout << "Here is the conjugation Matrix\n" << aComp.conjugate() << std::endl;
 	std::cout << "Here is the adjoint Matrix(a^*)\n" << aComp.adjoint() << std::endl;
 
+	std::cout << "Transpositon in place" << std::endl;
+	Eigen::MatrixXf inPlace(2, 3);
+	inPlace << 1, 2,5,
+		3, 4,3;
+	std::cout << "Original Matrices " << std::endl;
+	std::cout << inPlace << std::endl;
+	inPlace.transposeInPlace();
+	std::cout << "Using transpose in place " << std::endl;
+	std::cout << inPlace << std::endl;
 
+	Eigen::Matrix2d mat;
+	mat << 1, 2,
+		3, 4;
+	Eigen::Vector2d uVec(-1, 1), vVec(2, 0);
+	std::cout << "Show Current Matrix" << std::endl;
+	std::cout << mat << std::endl;
+	std::cout << "1.Show Current Vector" << std::endl;
+	std::cout << uVec << std::endl;
+	std::cout << "2.Show Current Vector" << std::endl;
+	std::cout << vVec << std::endl;
+	std::cout << "Here is mat*mat  : \n" << mat * mat << std::endl;
+	std::cout << "Here is mat*first vector  : \n" << mat *uVec << std::endl;
+	std::cout << "Here is vec^T*matrix  : \n" << uVec.transpose() *mat << std::endl;
+	std::cout << "Here is vec^T*matrix  : \n" << uVec.transpose() * mat << std::endl;
+	std::cout << "Let's mutiply  matrix by itself" << std::endl;
+	mat = mat * mat;
+	std::cout << mat << std::endl;
+
+	std::cout << "Dot Product and Cross Product" << std::endl;
+	Eigen::Vector3d vecbar(1, 2, 3);
+	Eigen::Vector3d sVecbar(0, 1, 2);
+	std::cout << "Dot Product \t: " << vecbar.dot(sVecbar) << std::endl;
+	std::cout << "Adjoint from first Vector " << vecbar.adjoint() << std::endl;
+	double dp = vecbar.adjoint() * sVecbar;
+	std::cout << "Dot Product via matrix product " <<dp<< std::endl;
+	std::cout << "Cross Product : \n" << vecbar.cross(sVecbar) << std::endl;
+
+	std::cout << "Basic  Arithmetic Reduction Operations" << std::endl;
+	Eigen::Matrix2d matSe;
+	matSe << 1, 2,
+		3, 4;
+	std::cout << "Show Current Matrix Value = \n" << matSe <<std::endl;
+	std::cout << "Here is mat.sum() = \n" << matSe.sum() << std::endl;
+	std::cout << "Here is mat.prod() = \n" << matSe.prod() << std::endl; //get by multiplying each element
+	std::cout << "Here is mat.mean() = \n" << matSe.mean() << std::endl;
+	std::cout << "Here is mat.minCoef() = \n" << matSe.minCoeff() << std::endl;
+	std::cout << "Here is mat.maxCoef() = \n" << matSe.maxCoeff() << std::endl;
+	//get by the sum of the diagonal coeffiecients
+	std::cout << "Here is mat.trace() = \n" << matSe.trace() << std::endl;
+
+	std::cout << "the minCoeff and maxCoeff functions -returning the cordinates" << std::endl;
+	Eigen::Matrix2d doubSec = Eigen::Matrix2d::Random();
+	std::ptrdiff_t i, j;
+	std::cout << "Here is the current matrix\t: \n" << doubSec << std::endl;
+	float minOfm = doubSec.minCoeff(&i, &j);
+	std::cout << "it's minimum coefficient";
+	std::cout << "It's minimum coefficients ( " << minOfm << ") is at position (" << i << "," << j << ")" << std::endl;
 
 
 	return 0;
